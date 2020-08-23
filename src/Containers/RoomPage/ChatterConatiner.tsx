7282890430bc141714b9cb5874cbe8d0bfc3e52d
bumsuk980119 <beom.kang17@gmail.com>
2020-08-23 13:34:05 +0900

    Ver 0.1.2

--- src/Containers/RoomPage/ChatterConatiner.tsx
+++ src/Containers/RoomPage/ChatterConatiner.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import ranStr from 'crypto-random-string';
 import Chatter from '../../Components/RoomPage/Chatter';
 import { ChatSocketData } from '../../api/socket';
+import { useDispatch } from 'react-redux';
+import { appendChat } from '../../Modules/chat';
 
 type ChatterContainerProps = {
   socket: SocketIOClient.Socket;
@@ -10,6 +12,8 @@ type ChatterContainerProps = {
 
 function ChatterContainer({ socket }: ChatterContainerProps) {
   const [chat, setChat] = useState('');
+  const dispatch = useDispatch();
+
   const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     setChat(event.target.value);
   }
@@ -19,6 +23,7 @@ function ChatterContainer({ socket }: ChatterContainerProps) {
       const id = ranStr({ length: 10 });
       const data: ChatSocketData = { chat, id };
       socket.emit('chat', data);
+      dispatch(appendChat(data.chat, data.id));
     }
     setChat('');
   }
